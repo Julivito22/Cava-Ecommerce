0028f1822743edfabea5c111f90849b722fd200b
JCMartinez <jcmtz.091@gmail.com>
2023-07-18 14:41:45 -0600

    feature - crear modelo Users y Products

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
@@ -30,12 +30,16 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
+/*
 const { Dogs } = sequelize.models;
 const { Temperaments } = sequelize.models;
+*/
+/**models */
+const {Users, Products} = sequelize.models; 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
-  
+
 };
